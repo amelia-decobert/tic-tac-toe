9286dbf7c631bc40b4f3e6194adb3b1effccadae
Mia <amelia.decobert@gmail.com>
2026-01-13 23:20:28 +0100

    set up value prop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // Create new component
-export function Square() {
-  return  <button className="square">1</button>
+// Square component receives value prop from board
+export function Square({ value }) {
+  // Display value inside button
+  return  <button className="square">{ value }</button>
 }
 
 // Create component
@@ -11,19 +13,20 @@ export default function Board() {
   {/* Group squares by line */}
   <div className="board-row">
     {/* Use new component Square */}
-    <Square />
-    <Square /> 
-    <Square /> 
+    {/* Pass value prop to each square component */}
+    <Square value="1" />
+    <Square value="2" /> 
+    <Square value="3" /> 
   </div>
   <div className="board-row">
-    <Square /> 
-    <Square /> 
-    <Square /> 
+    <Square value="4" /> 
+    <Square value="5" /> 
+    <Square value="6" /> 
   </div>
   <div className="board-row"> 
-    <Square /> 
-    <Square /> 
-    <Square /> 
+    <Square value="7" /> 
+    <Square value="8" /> 
+    <Square value="9" /> 
   </div>
   </>
   )
